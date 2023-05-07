3eccc1ba4e5e73e1524ed618d7cf40df141042a5
vivi88605 <VIVI88605@gmail.com>
2023-05-07 17:06:32 +0800

    set dynamic routing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const exphbs = require('express-handlebars')
 // require restaurant.json
 const restaurantData = require('./restaurant.json')
 
-//setting template engine
+// setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 // setting static files
@@ -15,7 +15,12 @@ app.use(express.static('public'))
 
 // routes setting
 app.get('/', (req, res) => {
-  res.render('index', { restaurantlist: restaurantData.results })
+  res.render('index', { restaurantList: restaurantData.results })
+})
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  console.log('req.params.restaurant_id: ', req.params.restaurant_id)
+  const ClickedRestaurant = restaurantData.results.find(item => item.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: ClickedRestaurant })
 })
 
 // start and listen on th Express server
